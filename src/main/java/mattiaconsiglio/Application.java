b5e2591577aa380a4bc5092d3a2c859ea0215cfe
Mattia Consiglio <37270576+mattia-consiglio@users.noreply.github.com>
2024-03-19 16:04:11 +0100

    Made exercise

--- src/main/java/mattiaconsiglio/Application.java
+++ src/main/java/mattiaconsiglio/Application.java
@@ -7,10 +7,10 @@ import jakarta.persistence.Persistence;
 
 public class Application {
 
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4dw3d2");
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestioneeventi");
 
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
-        System.out.println("Hello World!");
+
     }
 }
---
+++ src/main/java/mattiaconsiglio/entities/Evento.java
@@ -0,0 +1,55 @@
+package mattiaconsiglio.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "eventi")
+public class Evento {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String nome;
+    private LocalDate dataEvento;
+    private String descrizione;
+    @Enumerated(EnumType.STRING)
+    private TipoEvento tipoEvento;
+    private int numeroMassimoPartecipanti;
+
+    public Evento() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataEvento() {
+        return dataEvento;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public TipoEvento getTipoEvento() {
+        return tipoEvento;
+    }
+
+    public int getNumeroMassimoPartecipanti() {
+        return numeroMassimoPartecipanti;
+    }
+
+    public Evento(String nome, LocalDate dataEvento, String descrizione, TipoEvento tipoEvento, int numeroMassimoPartecipanti) {
+        this.nome = nome;
+        this.dataEvento = dataEvento;
+        this.descrizione = descrizione;
+        this.tipoEvento = tipoEvento;
+        this.numeroMassimoPartecipanti = numeroMassimoPartecipanti;
+    }
+}
